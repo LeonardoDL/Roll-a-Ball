59c7e3f51859326ba27db33818acb001e4c5ca9a
Leonardo <leo.del.lama@gmail.com>
2020-05-23 16:11:54 -0300

    Project commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -10f)
+        {
+            Debug.Log("You Lost");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 vector = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(vector * speed);
+    }
+}
---
+++ Assets/Scripts/Showcase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Showcase : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Debug.Log("This is a Log");
+        if (Input.GetKeyDown(KeyCode.S))
+            Debug.LogWarning("This is a Warning");
+        if (Input.GetKeyDown(KeyCode.D))
+            Debug.LogError("This is an Error");
+    }
+}
